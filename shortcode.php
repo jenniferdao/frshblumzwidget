57e3eebd8cc454b03f10c16b0d3411606de0b5b3
zubinkhan <zubin.khan@mail.utoronto.ca>
2016-03-29 17:00:18 -0400

    Updated file

--- shortcode.php
+++ shortcode.php
@@ -5,16 +5,13 @@
 * Description: A widget to display the yearly archives.
 * Plugin URI: http://phoenix.sheridanc.on.ca/~ccit3430
 * Author: Jennifer Dao & Zubin Khan
-* Author URI: http://phoenix.sheridanc.on.ca/~ccit3430
+* Author URI: http://phoenix.sheridanc.on.ca/~ccit3430*Hook the function register_my_post to function init.
 * Version: v1.0
 */
-//Calls the CSS file to the plugin
-function teampandajz_plugin_enqueue_scripts (){
-		wp_enqueue_style ('plugin', plugins_url ('frshblumz/css/freshblumzstyles.css')); 
-	} style.
-add_action( 'wp_enqueue_scripts','teampandajz_plugin_enqueue_scripts' );
+
 /*
 *Register the custom post type for the slider. This will also add an array of settings to the new custom post type.
+*Hook the function register_my_post to function init.
 */
 add_action('init','register_my_post');
 function register_my_post() {
@@ -45,15 +42,24 @@ function register_my_post() {
 			)
 		);
 }
-
+//Create a widget class teampandajz_widget
 class teampandajz_widget extends WP_Widget {
-
+//Class teampandajz_widget’s function that running when class is contructed
 	function __construct() {
 		parent::__construct('teampandajz_widget', __('teampandajz Widget', 'teampandajz_widget_domain'), array( 'description' => __( 'teampandajz widget', 'teampandajz_widget_domain' ), )
 		);
+		add_action( 'wp_enqueue_scripts', array( $this, 'register_plugin_styles' ) );
+	}
+	/**
+	 * Register and enqueue style sheet.
+	 */
+	public function register_plugin_styles() {
+		wp_register_style( 'flowerpost-plugin', plugins_url( '/home/ccit3430/public_html/wp-content/plugins/frshblumz/css' ) );
+		wp_enqueue_style( 'flowerpost-plugin' );
 	}
 
 	// Creating widget front-end
+	// Class teampandajz_widget’s function that creates the widget front-end. To display the widget in webpage.
 	public function widget( $args, $instance ) {
 	
 		$title = apply_filters( 'widget_title', $instance['title'] );
@@ -69,7 +75,7 @@ class teampandajz_widget extends WP_Widget {
 			$args = array('post_type'=>'flowerpost', 'posts_per_page'=>$number);
 			$query = new WP_Query($args);
 			if ($query ->have_posts() ) {
-				echo '<div style="height:20px">&nbsp;</div><div  style="margin-left:50px"><ul class="teampandajz_widget">';
+				echo '<div style="height:20px">&nbsp;</div><div class="flowerpostbox"><ul class="teampandajz_widget">';
 				$ct=0;
 				$col = 4;
 				$col = max(min($col, $number, $query->found_posts), 1);
@@ -83,8 +89,8 @@ class teampandajz_widget extends WP_Widget {
 					}
 					else
 						$typeflower = '<li style="width:'.intval(100/$col).'%; display:inline-block;float:left;text-align: center;">';
-					$typeflower .= '<div style=" margin:0px 40px; padding:2px;background-color:#e080e0;"><a href="' . get_permalink() . '">'. $image.'<br />';
-					$typeflower .= '<div style="height:60px;"><p style="vertical-align:middle;margin-bottom:0px;">' .get_the_title() . '</p></div></a></div></li>';
+					$typeflower .= '<div class="flowerpost"><a href="' . get_permalink() . '">'. $image.'<br />';
+					$typeflower .= '<div class="flowerposttext"><p>' .get_the_title() . '</p></div></a></div></li>';
 
 					echo $typeflower;
 				}
@@ -96,6 +102,7 @@ class teampandajz_widget extends WP_Widget {
 		}
 
 	// Widget Backend
+	// Class teampandajz_widget’s function that creating widget backend, include a settings form.
 	public function form( $instance ) {
 		if ( isset( $instance[ 'title' ] ) ) {
 			$title = $instance[ 'title' ];
@@ -121,7 +128,7 @@ class teampandajz_widget extends WP_Widget {
 <?php
 	}
 
-	// Updating widget replacing old instances with new
+	// Class teampandajz_widget’s function that updating widget, replacing old instances with new one.
 	public function update( $new_instance, $old_instance ) {
 		$instance = array();
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
@@ -142,18 +149,21 @@ function teampandajz_load_widget() {
 
 add_action( 'widgets_init', 'teampandajz_load_widget' );
 
-
+//  Hook function flowerpost_plugin_settings to function admin_init
 add_action( 'admin_init', 'flowerpost_plugin_settings' );
+// The function that register flowerpost settings.
 function flowerpost_plugin_settings() {
 	register_setting( 'flowerpost-plugin-settings', 'flowerpost_show_number' );
 	register_setting( 'flowerpost-plugin-settings', 'flowerpost_show_settype' );
 }
-
+// Hook function flowerpost_plugin_settings to admin_init
 add_action('admin_menu', 'flowerpost_plugin_menu');
+// The function that add a settings sub menu to Flowerpost menu
 function flowerpost_plugin_menu() {
 	add_submenu_page ( get_admin_page_parent ( $parent = '' ), 'flowerpost Plugin Settings', 'Settings', 'administrator', 'flowerpost-plugin-settings', 'flowerpost_plugin_settings_page');
 }
 
+//The function that create a settings form for flowerpost plugin
 function flowerpost_plugin_settings_page() {
 
 ?>
@@ -182,8 +192,9 @@ function flowerpost_plugin_settings_page() {
 <?php  
 
 }
-
+//Setup a short code flowerpost_shortcode
 add_shortcode('flowerpost_shortcode', 'flowerpost_shortcode' );
+//The function that shows the flower posts in webpage.
 function flowerpost_shortcode($stts){
 	$number = isset($stts['number']) ? $stts['number'] : get_option('flowerpost_show_number',3);
 	$col = isset($stts['colnum']) ? $stts['colnum'] : 4;
@@ -217,4 +228,13 @@ function flowerpost_shortcode($stts){
 	}
 }
 
+add_action( 'wp_enqueue_scripts', 'register_flowerpost_styles' );
+
+/**
+ * Register style sheet.
+ */
+function register_flowerpost_styles() {
+	wp_register_style( 'flowerpost-plugin', plugins_url( '/home/ccit3430/public_html/wp-content/plugins/frshblumz/css/style.css' ) );
+	wp_enqueue_style( 'flowerpost-plugin' );
+}
 ?>
